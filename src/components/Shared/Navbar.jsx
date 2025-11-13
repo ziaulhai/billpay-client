e871a2f880abe6707be3c536fc1f499628b6364c
ziaulhai <md.ziaulhai@gmail.com>
2025-11-13 20:36:47 +0600

    Navbar update

--- src/components/Shared/Navbar.jsx
+++ src/components/Shared/Navbar.jsx
@@ -7,6 +7,7 @@ import ThemeToggler from '../Shared/ThemeToggler';
 const Navbar = () => {
   const { user, logOut, loading } = useContext(AuthContext);
 
+  
   const handleLogout = () => {
     logOut()
       .then(() => {

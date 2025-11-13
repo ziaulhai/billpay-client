279bbead396c271e439256e527363dba000155d3
ziaulhai <md.ziaulhai@gmail.com>
2025-11-13 20:24:10 +0600

    Error page update

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -13,7 +13,9 @@ const ErrorPage = () => {
     <>
     <Navbar />
       <div className="min-h-screen flex flex-col items-center justify-center text-center bg-gray-100">
+     
       <h1 className="text-9xl font-extrabold text-red-600">404</h1>
+      <img src="https://i.ibb.co.com/TZDV6Qy/errorbill.png" className='w-[250px] h[350px]' alt="" />
       <p className="text-2xl font-semibold mb-4">Page Not Found</p>
       <Link to="/" className="btn btn-primary">
        Back to Home
